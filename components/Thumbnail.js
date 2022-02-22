206666c22554ded9d0aa6237a28a35d821b71079
Ahmedbenkhalifa <ahmedbenkhalifa1997@gmail.com>
2022-02-22 15:20:32 +0100

    update

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import React from "react";
-import { forwardRef } from "react";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 
-const Thumbnail = forwardRef(({ result },ref) => {
+const Thumbnail = ({ result }) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
         alt=""
@@ -20,7 +19,9 @@ const Thumbnail = forwardRef(({ result },ref) => {
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
+        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+          {result.title || result.original_name}
+        </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type} •`}{" "}
           {result.release_date || result.first_air_date} •{" "}
@@ -29,6 +30,6 @@ const Thumbnail = forwardRef(({ result },ref) => {
       </div>
     </div>
   );
-});
+};
 
 export default Thumbnail;
